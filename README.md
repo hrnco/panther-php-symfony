101f007b1f91fe10c2d674a74a93ad67d86b0c6b
Matej Hrnciarik <hrnciarik@aroha.sk>
2022-05-25 01:56:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Testovacie Prostredie
+
+## Spustenie testov
+
+```
+# spusti testy
+docker-compose run test ./bin/phpunit
+
+# spusti testy pre zabbix
+docker-compose run test ./bin/phpunit_boolean
+```
---
+++ example/tests/ArohaTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use App\Panther\UniversalTestCase;
+
+
+class ArohaTest extends UniversalTestCase
+{
+    public function testSomething(): void
+    {
+
+        // objekt browser
+        $client = $this->getBrowserClient();
+
+        // nacitanie homepage
+        $crawler = $client->request('GET', '');
+
+        // $this->takeScreenshot('homepage.png');
+
+        // overenie
+        $this->assertPageTitleContains('Aroha');
+        $this->assertSelectorTextContains('.footer-widgets-wrapper','Git repos');
+        $this->assertSelectorTextContains('.footer-widgets-wrapper','Kontakt');
+        $this->assertSelectorTextContains('.header-footer-group > div > aside > div > div.footer-widgets.column-two.grid-item > div > div > div > p:nth-child(1)','Matej');
+        $this->assertSelectorTextContains('div.post-inner.thin > div','Konzultácie');
+
+        // vypnutie browsera
+        $client->quit();
+
+    }
+
+}
